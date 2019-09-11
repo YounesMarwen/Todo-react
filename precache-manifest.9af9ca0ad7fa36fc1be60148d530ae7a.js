93390f7494023e9a4990910a898f902cf66c9cfa
YounesMarwen <younes.maroine@gmail.com>
2019-09-11 18:26:39 +0100

    Updates

---
+++ precache-manifest.9af9ca0ad7fa36fc1be60148d530ae7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18599a19f2d897427eef798790824108",
+    "url": "/YounesMarwen/Todo-react/index.html"
+  },
+  {
+    "revision": "195b14178cac3ad6de88",
+    "url": "/YounesMarwen/Todo-react/static/css/main.f44e8e9b.chunk.css"
+  },
+  {
+    "revision": "7581257c34097b6a9622",
+    "url": "/YounesMarwen/Todo-react/static/js/2.4ab7a279.chunk.js"
+  },
+  {
+    "revision": "195b14178cac3ad6de88",
+    "url": "/YounesMarwen/Todo-react/static/js/main.4fded6cd.chunk.js"
+  },
+  {
+    "revision": "3e791353f2550540f5c0",
+    "url": "/YounesMarwen/Todo-react/static/js/runtime~main.25d44e51.js"
+  }
+]);
