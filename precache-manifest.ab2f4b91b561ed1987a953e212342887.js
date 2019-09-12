fa58813cd6d5be973fe78755987f47349fd80021
YounesMarwen <younes.maroine@gmail.com>
2019-09-12 10:37:09 +0100

    Updates

---
+++ precache-manifest.ab2f4b91b561ed1987a953e212342887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916c2e353b06160b90d25827fa1dd42e",
+    "url": "/Todo-react/index.html"
+  },
+  {
+    "revision": "18a17c034c36b8f338ad",
+    "url": "/Todo-react/static/css/main.f44e8e9b.chunk.css"
+  },
+  {
+    "revision": "95214a16409b5ec3fe71",
+    "url": "/Todo-react/static/js/2.a1e66610.chunk.js"
+  },
+  {
+    "revision": "18a17c034c36b8f338ad",
+    "url": "/Todo-react/static/js/main.813e890c.chunk.js"
+  },
+  {
+    "revision": "288ce35a086d3f835ffa",
+    "url": "/Todo-react/static/js/runtime~main.e0ac4826.js"
+  }
+]);
