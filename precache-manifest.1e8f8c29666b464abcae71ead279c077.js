e85568da882128061a0d0cdc08a7ccc65dd91dd7
YounesMarwen <younes.maroine@gmail.com>
2019-09-12 10:29:18 +0100

    Updates

---
+++ precache-manifest.1e8f8c29666b464abcae71ead279c077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7feb656cb62f515488b6a89b42d2d5",
+    "url": "//Todo-react/index.html"
+  },
+  {
+    "revision": "37fb6750678b310df357",
+    "url": "//Todo-react/static/css/main.f44e8e9b.chunk.css"
+  },
+  {
+    "revision": "95214a16409b5ec3fe71",
+    "url": "//Todo-react/static/js/2.a1e66610.chunk.js"
+  },
+  {
+    "revision": "37fb6750678b310df357",
+    "url": "//Todo-react/static/js/main.9c855e16.chunk.js"
+  },
+  {
+    "revision": "083cfab7213975e593ed",
+    "url": "//Todo-react/static/js/runtime~main.f31b8465.js"
+  }
+]);
